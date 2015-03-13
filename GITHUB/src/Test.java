fa1495404f8a80108a113598ead71deade8d31db
MartinVoehringer <Martin.Voehringer@Student.Reutlingen-University.DE>
2015-03-13 18:39:31 +0100

    Ein neues projekt, ein neuer commit...

--- GITHUB/src/Test.java
+++ GITHUB/src/Test.java
@@ -3,5 +3,6 @@ public class Test {
 	public static void main(String[]args){
 		System.out.println("Hallo");
 		System.out.println("Hallo, wie geht´s?");
+		System.out.println("Uuuuuund ein neuer push");
 	}
 }
