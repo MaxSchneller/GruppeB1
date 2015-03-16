65f8e6ed9992654be2c2a2f952b70764b30a2fa8
Markus <Markus@Markus-Laptop>
2015-03-16 17:45:22 +0100

    Hab nen neuen Syso

--- GITHUB/src/Test.java
+++ GITHUB/src/Test.java
@@ -4,5 +4,7 @@ public class Test {
 		System.out.println("Hallo");
 		System.out.println("Hallo, wie geht´s?");
 		System.out.println("Uuuuuund ein neuer push");
+		
+		System.out.println("Klappt relativ gut oder??? Martin und Christop? Klapps bei euch auch?");
 	}
 }
