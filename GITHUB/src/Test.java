a670951605759f8ec7d254ab24dbc721b2eb9e24
Max Schneller <Max.Schneller@student.reutlingen-university.de>
2015-03-17 12:20:01 +0100

    neues Syso

--- GITHUB/src/Test.java
+++ GITHUB/src/Test.java
@@ -2,6 +2,7 @@
 public class Test {
 	public static void main(String[]args){
 		System.out.println("Hallo");
-		System.out.println("Hallo, wie geht´s?");
+		System.out.println("Hallo, wie gehtï¿½s?");
+		System.out.println("Hello");
 	}
 }
