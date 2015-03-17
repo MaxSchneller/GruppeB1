8612a86f0e1187f30fe3d675409d5132c9381043
Christoph <christoph.doerr@student.reutlingen-university.de>
2015-03-17 14:41:49 +0100

    JUnit Trivial und bisschen Spieler.klasse

--- MADN/src/JUnitMADN.java
+++ MADN/src/JUnitMADN.java
@@ -7,11 +7,14 @@ import org.junit.Test;
 public class JUnitMADN {
 	
 	protected Wuerfel w;
+	protected Spieler s;
 		
 	@Before
 	public void vorWuerfeln() {
 		System.out.println("Vor dem Würfeln");
 		w = new Wuerfel();
+		s = new Spieler("Max", FarbEnum.gelb,w);
+	
 	}
 
 	@Test
@@ -21,5 +24,17 @@ public class JUnitMADN {
 			assertTrue(gewuerfelteZahl > 0 && gewuerfelteZahl < 7);
 		}
 	}
+	@Test
+	public void testSpieler(){
+		String name=s.getName();
+		FarbEnum farbe=s.getFarbe();
+		
+		assertNotNull(name);
+		assertTrue(farbe instanceof FarbEnum); 
+		
+	}
 
+	
+	
 }
+ 
--- MADN/src/Spieler.java
+++ MADN/src/Spieler.java
@@ -2,10 +2,17 @@
 public class Spieler {
 	private String name;
 	private FarbEnum farbe;
+	private Wuerfel w;
 	
-	public Spieler(String name, FarbEnum farbe){
+	public Spieler(String name, FarbEnum farbe, Wuerfel w){
 		setName(name);
-		this.farbe = farbe;
+		this.setFarbe(farbe);
+		getWuerfel();
+		
+	}
+	
+	public Wuerfel getWuerfel(){
+		return w;
 	}
 	
 	public String getName(){
@@ -15,4 +22,15 @@ public class Spieler {
 	public void setName(String name){
 		this.name = name;
 	}
+
+	public FarbEnum getFarbe() {
+		return farbe;
+	}
+
+	public void setFarbe(FarbEnum farbe) {
+		this.farbe = farbe;
+	}
+
 }
+
+ 
