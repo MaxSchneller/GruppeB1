01db8ab7e8c7e66d617b82b45b16d2b057c6db7f
Markus <Markus@Markus-Laptop>
2015-03-17 13:14:37 +0100

    Die Testklasse mit Wuerfel JUnit Test

---
+++ MADN/src/JUnitMADN.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class JUnitMADN {
+	
+	protected Wuerfel w;
+		
+	@Before
+	public void vorWuerfeln() {
+		System.out.println("Vor dem Würfeln");
+		w = new Wuerfel();
+	}
+
+	@Test
+	public void testWuerfel() {
+		for (int i = 0 ; i <= 10 ; i++){
+			int gewuerfelteZahl = w.werfen();
+			assertTrue(gewuerfelteZahl > 0 && gewuerfelteZahl < 7);
+		}
+	}
+
+}
