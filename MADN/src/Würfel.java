f8ba60cef3641c9f4ef2fe0c036c29247d914d69
Markus <Markus@Markus-Laptop>
2015-03-17 13:19:30 +0100

    Zweites mal!
    JUnit

--- MADN/src/Würfel.java
+++
@@ -1,9 +0,0 @@
-
-public class Würfel {
-
-	public int werfen() {
-		int zufall = (int) (6 * Math.random()) + 1;
-		System.out.println("Es wurde " + zufall + " gewuerfelt!");
-		return zufall;
-		}
-}
