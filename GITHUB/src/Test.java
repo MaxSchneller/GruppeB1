85aa1d331595b6d6032b67fc9f0df893af867275
Max Schneller <Max.Schneller@student.reutlingen-university.de>
2015-03-17 12:38:12 +0100

    oho

--- GITHUB/src/Test.java
+++ GITHUB/src/Test.java
@@ -2,14 +2,10 @@
 public class Test {
 	public static void main(String[]args){
 		System.out.println("Hallo");
-		System.out.println("Hallo, wie geht�s?");
+		System.out.println("Hallo, wie gehts?");
 		System.out.println("Hello");
 		System.out.println("Uuuuuund ein neuer push");
 		
 		System.out.println("Klappt relativ gut oder??? Martin und Christop? Klapps bei euch auch?");
-		System.out.println("So ich bin auch hier :-*");
-		
-		System.out.println("Hallo ich auch");
 	}
-	
 }
