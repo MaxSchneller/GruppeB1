a1db37d41275fd741ce2817db60a629ccec649ea
Christoph <christoph.doerr@student.reutlingen-university.de>
2015-04-23 16:16:47 +0200

    GUI änderungen Würfel und Würfel knoppes

--- MADN/src/GUI/madnGUI.java
+++ MADN/src/GUI/madnGUI.java
@@ -1,6 +1,12 @@
 package GUI;
-	import java.awt.BorderLayout;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridLayout;
 import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -8,6 +14,9 @@ import java.io.IOException;
 import java.net.URL;
 
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -22,87 +31,81 @@ import javax.swing.JTextField;
 
 import java.awt.Image;
 
-
-
-
-
-
-
-
 public class madnGUI {
 
+	public static void main(String[] args) throws IOException {
 
-
-	
-		 public static void main(String [] args) throws IOException{
-			 
-		
 		File imageFile = new File("bilder/madn.jpg");
 		BufferedImage madn = ImageIO.read(imageFile);
-		
+
 		File imageFile2 = new File("bilder/wuerfel.png");
 		BufferedImage wuerfel = ImageIO.read(imageFile2);
+
+		ImageIcon bild = new ImageIcon(wuerfel);
+		bild.setImage(bild.getImage().getScaledInstance(80, 80,
+				Image.SCALE_DEFAULT));
+
+		Color color = Color.BLACK;
+		JFrame jf = new JFrame("Mensch-Ärgere-Dich-Nicht");
 		
-		Color color=Color.BLACK;
-				
-		JFrame jf=new JFrame("Mensch-Ärgere-Dich-Nicht");
 		
-		JMenuItem jmi=new JMenuItem("Speichern");
-		JMenuItem jmi1=new JMenuItem("Laden");
-		JMenuItem jmi2=new JMenuItem("Beenden");
-		JPanel jp_north=new JPanel();
-		JPanel jp_south=new JPanel();
-		JPanel jp_west=new JPanel();
-		JPanel jp_east=new JPanel();
-		JPanel jp_center=new JPanel();
 		
-		JButton buttonWuerfel=new JButton("Wuerfeln");
-		JMenuBar jm=new JMenuBar();
-		JMenu jMenu=new JMenu("Datei");
-		JMenu jMenu2=new JMenu("Hilfe");
-		JTextArea console=new JTextArea(1,60);
+		
+		JMenuItem jmi = new JMenuItem("Speichern");
+		JMenuItem jmi1 = new JMenuItem("Laden");
+		JMenuItem jmi2 = new JMenuItem("Beenden");
+		JPanel jp_north = new JPanel();
+		JPanel jp_south = new JPanel();
+		JPanel jp_west = new JPanel();
+		JPanel jp_east = new JPanel();
+		JPanel jp_center = new JPanel();
+
+		JButton buttonWuerfel = new JButton("Wuerfeln");
+		
+		JMenuBar jm = new JMenuBar();
+		JMenu jMenu = new JMenu("Datei");
+		JMenu jMenu2 = new JMenu("Hilfe");
+		JTextArea console = new JTextArea(1, 60);
 		console.setEditable(false);
-		jp_west.setBackground(color);
-		jp_east.setBackground(color);
-		jp_south.setBackground(color);
-		jp_center.setBackground(color);
-		jp_north.setBackground(color);
+
 		jf.setLayout(new BorderLayout());
 		jf.add(jp_north, BorderLayout.NORTH);
 		jf.add(jp_south, BorderLayout.SOUTH);
 		jf.add(jp_east, BorderLayout.EAST);
 		jf.add(jp_west, BorderLayout.WEST);
 		jf.add(jp_center, BorderLayout.CENTER);
-		jp_east.add(buttonWuerfel);
-		jp_center.add(new JLabel(new ImageIcon(madn)));
 		jf.setJMenuBar(jm);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setVisible(true);
+		//jp_center.setBorder(BorderFactory.createLineBorder(Color.WHITE));
+		jp_west.setBackground(color);
+		jp_east.setBackground(color);
+		jp_south.setBackground(color);
+		jp_center.setBackground(color);
+		jp_north.setBackground(color);
+		
+		jp_center.add(new JLabel(new ImageIcon(madn)));
+		jp_south.add(console);
+		jp_east.setPreferredSize(new Dimension(150,1));
+		jp_east.setLayout(new BorderLayout());
+		jp_east.add(buttonWuerfel, BorderLayout.CENTER);
+		jp_west.setPreferredSize(new Dimension(150,1));
+		
+		jp_west.setLayout(new BorderLayout());
+		
+		
+
+		jp_west.add(new JLabel(bild), BorderLayout.EAST);
+
 		jm.add(jMenu);
 		jm.add(jMenu2);
 		jMenu.add(jmi);
 		jMenu.add(jmi1);
 		jMenu.add(jmi2);
-		jp_south.add(console);
-		
-		
-		ImageIcon bild=new ImageIcon(wuerfel);
-		bild.setImage(bild.getImage().getScaledInstance(80, 80, Image.SCALE_DEFAULT));
-		
-		
-		
-		jp_west.add(new JLabel(bild));
-		
-		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		jf.setVisible(true);
-		
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		int xSize = ((int) tk.getScreenSize().getWidth());
-		int ySize = ((int) tk.getScreenSize().getHeight());
-		jf.setSize(xSize,ySize);
-		
-		 }
-		
 
 		
+		jf.setSize(1000, 1000);
+
 	}
 
-
+}
