a97b3e8abf24542f3eaded938474f330818f7c2d
Markus <Markus@Markus-Laptop>
2015-05-06 13:10:32 +0200

    I hau da so a Fenschdole gschrieba, mit deam mo so a neumodisch gruschd macha kah. SO an Post klombatsch. hoißt glaub Mail

---
+++ MADN/src/GUI/MailGUI.java
@@ -0,0 +1,89 @@
+package GUI;
+
+import java.awt.Dialog;
+
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextArea;
+
+public class MailGUI {
+
+	private JDialog jd;
+	private Box box;
+	private Dialog jd2;
+
+	private JLabel jl2;
+
+	private JPanel jp3;
+	private JLabel eMailAdresse;
+	private JTextArea jTA;
+
+	private ButtonGroup anhang;
+	private JRadioButton welcheDatei;
+	private JRadioButton welcheDatei2;
+
+	private JButton sendeButton;
+
+	public void fensterFuerMailVersenden() {
+
+		jd = new JDialog();
+		box = new Box(BoxLayout.Y_AXIS);
+		jd2.setTitle("Spielstand per Mail versenden");
+
+		// Fuer was ist das Fester?
+		JPanel jp2 = new JPanel();
+		jl2 = new JLabel(
+				"Versendnen Sie Ihren Spielstand per Mail an wen Sie wollen.");
+		jp2.add(jl2);
+		box.add(jp2);
+
+		// Abstand
+		box.add(new JPanel());
+
+		// Email Text eingeben
+		jp3 = new JPanel();
+		eMailAdresse = new JLabel("EMail Adresse des Empfängers: ");
+
+		String emailText = "Hallo, hier ist mein Spielstand meines Mensch Ärger dich nicht Spiels. Liebe Grüße ";
+				//+ DialogGUI.getName();
+		jTA = new JTextArea(emailText, 5, 30);
+		jp3.add(jTA);
+		box.add(jp3);
+
+		// Abstand
+		box.add(new JPanel());
+
+		// Datei auswaehlen
+		JPanel jp5 = new JPanel();
+		anhang = new ButtonGroup();
+		welcheDatei = new JRadioButton("Serialisierte Datei", true);
+		welcheDatei2 = new JRadioButton("PDF Datei", false);
+
+		anhang.add(welcheDatei);
+		anhang.add(welcheDatei2);
+		jp5.add(welcheDatei);
+		jp5.add(welcheDatei2);
+		box.add(jp5);
+
+		// Abstand
+		box.add(new JPanel());
+
+		// Sendebutton
+		sendeButton = new JButton("Senden");
+		// sendeButton.addActionListener(this.maingui.getEventHandler());
+		box.add(sendeButton);
+
+		jd.setContentPane(box);
+		jd.pack();
+		jd.setLocationRelativeTo(null);
+		jd.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		jd.setVisible(true);
+	}
+
+}
